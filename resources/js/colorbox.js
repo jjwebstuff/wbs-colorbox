04e5a7b8912618a7aa89ed67894fda096d1558c4
Siddall, Jake(jsiddall) <Jake.Siddall@cmegroup.com>
2014-05-29 16:09:56 -0500

    updates portfolio.html with isotope functionality

--- resources/js/colorbox.js
+++ resources/js/colorbox.js
@@ -1,5 +1,27 @@
   $(window).load(function() {
+  	
+  	// Initialize the flexslider
     $('.flexslider').flexslider({
 
+    });
+    //Initialize Isotope
+    var $container = $('#portfolio-container'), $filters = $("#filters a");
+
+    // $container.imagesLoaded(function() {
+        $container.isotope({
+            itemSelector: '.project',
+            masonry: {
+                columnWidth: 0
+            }
+        });
+    // });
+
+    // filter items when filter link is clicked
+    $filters.click(function() {
+        $filters.parent().removeClass("active");
+        $(this).parent().addClass("active");
+        var selector = $(this).data('filter');
+        $container.isotope({filter: selector});
+        return false;
     });
   });
